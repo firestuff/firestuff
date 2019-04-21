ae6b4995c9f88f59611e6e17946f65e4637e89f1
Ian Gulliver <ian@flamingcow.tv>
2019-04-21 18:06:52 +0000

    Conserve work using mtime

--- markdown/build-html.sh
+++ markdown/build-html.sh
@@ -8,11 +8,15 @@ for FILE in $BASEDIR/*.md; do
 		continue
 	fi
 	OUTNAME=${BASENAME%.md}.html
+	OUTPATH=$BASEDIR/../$OUTNAME
+	if [[ -e $OUTPATH && $OUTPATH -nt $FILE ]]; then
+		continue
+	fi
 	echo "$BASENAME -> $OUTNAME"
 	TEMP=$(tempfile --dir=$BASEDIR --mode=0644 --suffix=.tmp)
 	markdown $FILE > $TEMP
-	diff -ud --color=always $BASEDIR/../$OUTNAME $TEMP || true
-	mv $TEMP $BASEDIR/../$OUTNAME
+	diff -ud --color=always $OUTPATH $TEMP || true
+	mv $TEMP $OUTPATH
 done
 
 printf "\033[0;32mDONE\033[0m\n"
