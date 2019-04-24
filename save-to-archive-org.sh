34195a328ec565cbb6437478cb3e73fcf224e8b6
Ian Gulliver <ian@flamingcow.tv>
2019-04-24 02:23:49 +0000

    Archive markdown files

--- save-to-archive-org.sh
+++ save-to-archive-org.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -e
 
 TMPPATH=$(mktemp --directory)
-
 pushd $TMPPATH > /dev/null
 
 URLS=$(wget --spider --recursive --no-verbose https://firestuff.org/ 2>&1 | grep URL: | sed 's/.*URL: *//; s/ .*//')
+
+popd > /dev/null
+rm --recursive --force $TMPPATH
 
 for URL in $URLS; do
 	wget -O /dev/null --no-verbose https://web.archive.org/save/$URL
 done
 
-popd > /dev/null
+SCRIPTBASE=$(dirname $0)
 
-rm --recursive --force $TMPPATH
+for MD in $SCRIPTBASE/markdown/2*.md; do
+	FILENAME=$(basename $MD)
+	wget -O /dev/null --no-verbose https://web.archive.org/save/https://firestuff.org/markdown/$FILENAME
+done
